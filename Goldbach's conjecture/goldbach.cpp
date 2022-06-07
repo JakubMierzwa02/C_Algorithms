588c0a20e9e3161465b60d8ecd963e1c0f5b8259
Jakub <102422732+JakubMierzwa02@users.noreply.github.com>
2022-06-07 23:39:12 +0200

    Goldbach's conjecture

---
+++ Goldbach's conjecture/goldbach.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+bool isPrime(int n)
+{
+    int d = 2;
+    if (n < 2)
+        return false;
+    while (d * d <= n)
+    {
+        if (n%d == 0)
+            return false;
+        d++;
+    }
+    return true;
+}
+
+void goldbach(int n)
+{
+    for (int i = 2; i * 2 < n; i++)
+        for (int j = 2; j < n; j++)
+            if (isPrime(i) && isPrime(j) && i + j == n)
+                std::cout << i << " + " << j << " = " << n << '\n';
+}
+
+int main()
+{
+    int n;
+    std::cout << "Enter an even number: ";
+    std::cin >> n;
+
+    if (n%2 != 0)
+        std::cout << "Number must be even! \n";
+    else
+        goldbach(n);
+
+    return 0;
+}
